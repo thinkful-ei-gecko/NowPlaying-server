8e7e480e81a98aa3f5247b994879ec4131594ecb
preet-singh <preetsingh13@gmail.com>
2019-11-26 11:13:33 -0600

    cleaned cache and minor refactor

--- src/app.js
+++ src/app.js
@@ -4,11 +4,12 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const errorHandler = require('./middleware/error-handler');
 
 const app = express();
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
-//NODE_ENV = will be production by default on heroku 
-app.use(morgan(morganOption));//want to use a different morgan depending on dev env
+
+app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
@@ -16,16 +17,7 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello, world!')
 });
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if(NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else{
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;
 
