ba1c267388be17fb9c2d125d699ed4bba9253614
preet-singh <preetsingh13@gmail.com>
2019-11-26 10:51:39 -0600

    setup updates

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/',
+    JWT_SECRET: process.env.JWT_SECRET || 'secret',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
   }
   
