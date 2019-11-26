28d71d61fbab4c0b2c85e0d933cad83577b38099
preet-singh <preetsingh13@gmail.com>
2019-11-26 11:42:26 -0600

    auth and user route update

--- src/app.js
+++ src/app.js
@@ -5,13 +5,17 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const errorHandler = require('./middleware/error-handler');
+const authRouter = require('./auth /auth-router');
+const userRouter = require('./user/user-router');
 
 const app = express();
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
-
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
+
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello, world!')
