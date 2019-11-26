6f732df59ff4aca6fb2a8c2ae7a1b4a8aaeb925c
preet-singh <preetsingh13@gmail.com>
2019-11-26 12:58:26 -0600

    started on DB management - migrations and seeds

---
+++ migrations/001.do.create_user.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "user" (
+  "id" SERIAL PRIMARY KEY,
+  "username" TEXT NOT NULL UNIQUE,
+  "password" TEXT NOT NULL,
+  "name" TEXT NOT NULL,
+  "email" NVARCHAR(320) NOT NULL UNIQUE
+);
---
+++ seeds/seed.tables.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+TRUNCATE
+    "user";
+
+INSERT INTO "user"("id", "username", "name", "password", "email")
+VALUES
+    (
+        1, 
+        'demo',
+        'Demo User',
+        -- password = 'pass,
+        '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG',
+        'demo@email.com'
+    )
+
+SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
+COMMIT;
