b0389981161974a8114c227c137e06df9c4e8fbd
preet-singh <preetsingh13@gmail.com>
2019-11-27 15:07:18 -0600

    added in comments route to app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const errorHandler = require('./middleware/error-handler');
 const authRouter = require('./auth /auth-router');
 const userRouter = require('./user/user-router');
 const mainRouter = require('./main /main-router');
+const commentsRouter = require ('./comments/comments-router');
 
 const app = express();
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
@@ -18,6 +19,7 @@ app.use(helmet());
 app.use('/api/auth', authRouter); //login
 app.use('/api/user', userRouter); //registration
 app.use('/api/main', mainRouter); //main
+app.use('/api/comments', commentsRouter); //comments
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello, world!')
