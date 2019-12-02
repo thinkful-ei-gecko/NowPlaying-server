fe8d65c1e99dc955bea3efae7c954420fe26fc3b
preet-singh <preetsingh13@gmail.com>
2019-12-02 16:52:49 -0600

    started on auth testing

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,91 +1,121 @@
-const auth = require('../src/auth/auth-router');
-
-const knex = require('knex')
 const jwt = require('jsonwebtoken')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-//note for Preet anything that says pup or puppies is from an old project
-//need to re-adjust the tes to fit this application first then pass second :)
+describe('Auth Endpoints', function () {
+  let db
 
-describe('Auth Endpoints', function() {
-    let db
+  const testUsers = helpers.makeUsersArray()
+  const testUser = testUsers[0]
 
-    const { testUsers } = helpers.makePupsFixtures()
-    const testUser = testUsers[0]
+  before('make knex instance', () => {
+    db = helpers.makeKnexInstance()
+    app.set('db', db)
+  })
 
-    before('make knex instance', () => {
-        db = knex({
-            client: 'pg',
-            connection: process.env.DATABASE_URL,
-        })
-        app.set('db', db)
+  after('disconnect from db', () => db.destroy())
+
+  before('cleanup', () => helpers.cleanTables(db))
+
+  afterEach('cleanup', () => helpers.cleanTables(db))
+
+  /**
+   * @description Get token for login
+   **/
+  describe(`POST /api/auth/token`, () => {
+    beforeEach('insert users', () =>
+      helpers.seedUsers(
+        db,
+        testUsers,
+      )
+    )
+
+    const requiredFields = ['username', 'password']
+
+    requiredFields.forEach(field => {
+      const loginAttemptBody = {
+        username: testUser.username,
+        password: testUser.password,
+      }
+
+      it(`responds with 400 required error when '${field}' is missing`, () => {
+        delete loginAttemptBody[field]
+
+        return supertest(app)
+          .post('/api/auth/token')
+          .send(loginAttemptBody)
+          .expect(400, {
+            error: `Missing '${field}' in request body`,
+          })
+      })
     })
 
-    after('disconnet from db', () => db.destroy())
+    it(`responds 400 'invalid username or password' when bad username`, () => {
+      const userInvalidUser = { username: 'user-not', password: 'existy' }
+      return supertest(app)
+        .post('/api/auth/token')
+        .send(userInvalidUser)
+        .expect(400, { error: `Incorrect username or password` })
+    })
 
-    before('cleanup', () => helpers.clearTables(db))
+    it(`responds 400 'invalid username or password' when bad password`, () => {
+      const userInvalidPass = { username: testUser.username, password: 'incorrect' }
+      return supertest(app)
+        .post('/api/auth/token')
+        .send(userInvalidPass)
+        .expect(400, { error: `Incorrect username or password` })
+    })
 
-    afterEach('cleanup', () => helpers.clearTables(db))
-
-    describe(`POST /api/auth/login`, () => {
-        beforeEach('insert users', () => helpers.seedUsers(db, testUsers))
-
-        const requiredFields = ['user_name', 'password']
-
-        requiredFields.forEach(field => {
-            const loginAttemptBody = {
-                user_name: testUser.user_name,
-                password: testUser.password
-            }
-
-            it(`responds with 400 required error when '${field}' is missing`, () => {
-                delete loginAttemptBody[field]
-
-                return supertest(app)
-                    .post('/api/auth/login')
-                    .send(loginAttemptBody)
-                    .expect(400, {
-                        error: `Missing '${field}' in request body`,
-                    })
-            })
-        })
-
-        it(`responds 400 'invalid user_name or password' when bad user_name`, () => {
-        const userInvalidUser = { user_name: 'user-not', password: 'existy' }
-        return supertest(app)
-            .post('/api/auth/login')
-            .send(userInvalidUser)
-            .expect(400, { error: `Incorrect user_name or password` })
-        })
-    
-        it(`responds 400 'invalid user_name or password' when bad password`, () => {
-        const userInvalidPass = { user_name: testUser.user_name, password: 'incorrect' }
-        return supertest(app)
-            .post('/api/auth/login')
-            .send(userInvalidPass)
-            .expect(400, { error: `Incorrect user_name or password` })
-        })
-    
-        it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
-        const userValidCreds = {
-            user_name: testUser.user_name,
-            password: testUser.password,
+    it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
+      const userValidCreds = {
+        username: testUser.username,
+        password: testUser.password,
+      }
+      const expectedToken = jwt.sign(
+        { user_id: testUser.id, name: testUser.name },
+        process.env.JWT_SECRET,
+        {
+          subject: testUser.username,
+          expiresIn: process.env.JWT_EXPIRY,
+          algorithm: 'HS256',
         }
-        const expectedToken = jwt.sign(
-            { user_id: testUser.id },
-            process.env.JWT_SECRET,
-            {
-            subject: testUser.user_name,
-            algorithm: 'HS256',
-            }
-        )
-        return supertest(app)
-            .post('/api/auth/login')
-            .send(userValidCreds)
-            .expect(200, {
-            authToken: expectedToken,
-            })
+      )
+      return supertest(app)
+        .post('/api/auth/token')
+        .send(userValidCreds)
+        .expect(200, {
+          authToken: expectedToken,
         })
     })
+  })
+
+  /**
+   * @description Refresh token
+   **/
+  describe(`PATCH /api/auth/token`, () => {
+    beforeEach('insert users', () =>
+      helpers.seedUsers(
+        db,
+        testUsers,
+      )
+    )
+
+    it(`responds 200 and JWT auth token using secret`, () => {
+      const expectedToken = jwt.sign(
+        { user_id: testUser.id, name: testUser.name },
+        process.env.JWT_SECRET,
+        {
+          subject: testUser.username,
+          expiresIn: process.env.JWT_EXPIRY,
+          algorithm: 'HS256',
+        }
+      )
+      return supertest(app)
+        .put('/api/auth/token')
+        .set('Authorization', helpers.makeAuthHeader(testUser))
+        .expect(200, {
+          authToken: expectedToken,
+        })
+    })
+  })
 })
---
+++ test/test-helpers.js
@@ -0,0 +1,115 @@
+const knex = require('knex')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+/**
+ * create a knex instance connected to postgres
+ * @returns {knex instance}
+ */
+function makeKnexInstance() {
+  return knex({
+    client: 'pg',
+    connection: process.env.TEST_DATABASE_URL,
+  })
+}
+
+/**
+ * create a knex instance connected to postgres
+ * @returns {array} of user objects
+ */
+function makeUsersArray() {
+  return [
+    {
+      id: 1,
+      username: 'test-user-1',
+      password: 'password',
+      email: 'testemail1@email.com'
+    },
+    {
+      id: 2,
+      username: 'test-user-2',
+      password: 'password',
+      email: 'testemail1@email.com'
+    },
+  ]
+}
+
+/**
+ * make a bearer token with jwt for authorization header
+ * @param {object} user - contains `id`, `username`
+ * @param {string} secret - used to create the JWT
+ * @returns {string} - for HTTP authorization header
+ */
+function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
+  const token = jwt.sign({ user_id: user.id }, secret, {
+    subject: user.username,
+    algorithm: 'HS256',
+  })
+  return `Bearer ${token}`
+}
+
+/**
+ * remove data from tables and reset sequences for SERIAL id fields
+ * @param {knex instance} db
+ * @returns {Promise} - when tables are cleared
+ */
+function cleanTables(db) {
+    return db.transaction(trx =>
+      trx.raw(
+        `TRUNCATE
+            "user",
+            "category",
+            "movies",
+            "tv_shows",
+            "podcasts",
+            "books",
+            "movie_comments",
+            "tv_show_comments",
+            "podcast_comments",
+            "book_comments"`
+        )
+        .then(() =>
+          Promise.all([
+            trx.raw(`ALTER SEQUENCE user_id_seq minvalue 0 START WITH 1`),
+            trx.raw(`ALTER SEQUENCE category_id_seq minvalue 0 START WITH 1`),
+            trx.raw(`ALTER SEQUENCE movies_id_seq minvalue 0 START WITH 1`),
+            trx.raw(`ALTER SEQUENCE tv_shows_id_seq minvalue 0 START WITH 1`),
+            trx.raw(`ALTER SEQUENCE podcasts_id_seq minvalue 0 START WITH 1`),
+            trx.raw(`ALTER SEQUENCE books_id_seq minvalue 0 START WITH 1`),
+            trx.raw(`ALTER SEQUENCE movie_comments_id_seq minvalue 0 START WITH 1`),
+            trx.raw(`ALTER SEQUENCE tv_show_comments_id_seq minvalue 0 START WITH 1`),
+            trx.raw(`ALTER SEQUENCE podcast_comments_id_seq minvalue 0 START WITH 1`),
+            trx.raw(`ALTER SEQUENCE book_comments_id_seq minvalue 0 START WITH 1`),
+          ])
+        )
+    )
+  }
+/**
+ * insert users into db with bcrypted passwords and update sequence
+ * @param {knex instance} db
+ * @param {array} users - array of user objects for insertion
+ * @returns {Promise} - when users table seeded
+ */
+function seedUsers(db, users) {
+  const preppedUsers = users.map(user => ({
+    ...user,
+    password: bcrypt.hashSync(user.password, 1)
+  }))
+  return db.transaction(async trx => {
+    await trx.into('user').insert(preppedUsers)
+
+    await trx.raw(
+      `SELECT setval('user_id_seq', ?)`,
+      [users[users.length - 1].id],
+    )
+  })
+}
+
+
+module.exports = {
+  makeKnexInstance,
+  makeUsersArray,
+  makeAuthHeader,
+  seedUsers,
+  cleanTables,
+}
