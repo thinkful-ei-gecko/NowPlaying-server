f9e52640521998c5065a54f121c81e313bfc5e6a
preet-singh <preetsingh13@gmail.com>
2019-12-05 16:25:26 -0600

    minor changes to main to reflect movie migration

--- src/main/main-router.js
+++ src/main/main-router.js
@@ -40,8 +40,8 @@ mainRouter
       .catch(next);
   })
   .post(requireAuth, jsonParser, (req,res,next) => {
-    const { title, event_description, id, media_runtime, release_date, genre, imdb_rating, mpaa_rating, poster } = req.body;
-    const newThread = { title, event_description, id, media_runtime, release_date, genre, imdb_rating, mpaa_rating, poster };
+    const { title, event_description, media_runtime, release_date, genre, imdb_rating, mpaa_rating, poster, movie_id } = req.body;
+    const newThread = { title, event_description, media_runtime, release_date, genre, imdb_rating, mpaa_rating, poster, movie_id };
 
     if(!title) {
       return res.status(400).json( {error: 'Title is required'} );
