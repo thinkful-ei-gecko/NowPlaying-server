d1117264f23002ee4877f0a38005f02d93b138a4
preet-singh <preetsingh13@gmail.com>
2019-12-06 13:36:19 -0600

    date created updated for happening route

--- test/comments-endpoints.spec.js
+++ test/comments-endpoints.spec.js
@@ -1,2 +1,28 @@
-const app = require('../src/app')
-const helpers = require('./test-helpers')
+const jwt = require('jsonwebtoken');
+const app = require('../src/app');
+const helpers = require('./test-helpers');
+
+
+describe('Comment Endpoints', function () {
+    let db
+  
+    const testUsers = helpers.makeUsersArray()
+    const testUser = testUsers[0]
+  
+    before('make knex instance', () => {
+      db = helpers.makeKnexInstance()
+      app.set('db', db)
+    })
+  
+    after('disconnect from db', () => db.destroy())
+  
+    before('cleanup', () => helpers.cleanTables(db))
+  
+    afterEach('cleanup', () => helpers.cleanTables(db))
+
+    /**
+    * @description Posts a user 
+    **/
+    
+
+})
